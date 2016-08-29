5c10be4bda343548105d722cfe2843fa2cdda3d9
zengXianShu <zengxiangshu0@163.com>
2016-08-29 11:24:47 +0800

    edit

--- PodDemo/Pods/Target Support Files/Pods-PodDemo/Pods-PodDemo-frameworks.sh
+++ PodDemo/Pods/Target Support Files/Pods-PodDemo/Pods-PodDemo-frameworks.sh
@@ -84,10 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Kingfisher/Kingfisher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ZMWaveView/ZMWaveView.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Kingfisher/Kingfisher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ZMWaveView/ZMWaveView.framework"
 fi
